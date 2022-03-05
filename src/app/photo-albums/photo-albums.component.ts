33044c9ba886b8ecce2d3d7cd0c08c15df0b710a
mamta <mamta81194@gmail.com>
2022-03-05 20:53:35 +0530

    update search bar in albums

--- src/app/photo-albums/photo-albums.component.ts
+++ src/app/photo-albums/photo-albums.component.ts
@@ -12,75 +12,22 @@ export class PhotoAlbumsComponent implements OnInit {
 
   allPhotoAlbums: any = [];
   albums:any = []
-  selectedLevel:any;
-  selectedImage = new FormControl([]);
-  filteredImage: string = ''
-  searchValue: string = ''
+  albumTitle:string=""
 
-  selected(){    
-    console.log(this.selectedLevel)
-  }
   ngOnInit(): void {
     this.postService.getPhotoAlbumn().subscribe((resp: any) => {
       this.allPhotoAlbums = resp?.splice(0, 10);
       this.albums = this.allPhotoAlbums;
     })
   }
-
-  searchHandler(event: Event) {
-    const val = (event.target as HTMLInputElement).value;
-    const res = this.allPhotoAlbums.filter((photo: any) => photo.title.substring(0, val.length) === val)
-    console.log(res);
-    this.completeDataList(event)
-  }
-
-  fillDataListIfEmpty(event: Event) {
-    const inputTag = document.getElementById('languages') as HTMLInputElement;
-    if (!inputTag.innerHTML) {
-      // let event :Event  = {target : {}};
-      this.completeDataList(event);
-    }
-  }
-  completeDataList(e: Event) {
-    const target = (e.target as HTMLInputElement);
-    const inputTag = document.getElementById('languages') as HTMLInputElement;
-
-    const fill = (val: any) => inputTag.innerHTML = val;
-    if (!target.value) {
-      fill(this.allPhotoAlbums.reduce((sum: any, [html]: any) => sum + html, ''));
-    } else if (!(e instanceof InputEvent)) {
-      target.blur();
-    } else {
-      const inputValue = target.value.toLowerCase();
-      let result = '';
-      for (const [html, valuePattern] of this.allPhotoAlbums) {
-        if (!valuePattern.indexOf(inputValue)) {
-          result += html;
-        } else if (result) {
-          break;
-        }
-      }
-      fill(result);
-    }
-  }
-  onKeydown(e : Event){
-    const val = (e.target as HTMLInputElement).value;
-    console.log(val);
-  }
-
   getFilteredImage(event: any){
-    this.filteredImage = event
-    console.log(this.selectedImage);
-    if(!this.selectedImage.value.length){
+    const value = (event.target as HTMLInputElement).value;
+    let albumData = this.allPhotoAlbums.find((album:any) => album?.title === value);
+    if(!albumData){
       this.allPhotoAlbums = this.albums
     }else{
-      const res = this.allPhotoAlbums.filter((img: any) => img.id == this.selectedImage.value)
+      const res = this.allPhotoAlbums.filter((img: any) => img.id == albumData.id)
       this.allPhotoAlbums = res
     }
-   
-  }
-
-  gettitle(event: any){
-this.filteredImage = event.target.value
   }
 }
